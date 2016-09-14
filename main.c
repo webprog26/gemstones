5a102c380e37bf63884a7b8dd7a3c6358ce09c44
webprog26 <webprog26@gmail.com>
2016-09-14 11:12:13 +0300

    Final test version

--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 
 #define MAXLENGTH 100
+#define ALPHABET_LENGTH 26
 
+#define SMALL_A_ACII_INDEX 97
 int indexOf(char* s, char c)
 {
     char* sp = s;
@@ -61,9 +63,15 @@ int main()
     */
 
     //test Strings
+    /*
     char* s = "mhydccqrbddfyfoehmkfrbwebcassnqbydblcwrdbnaphlljqolegyxsprmgerjxxckkaxrkmqzdwawpbopdoikhlcof";
     char* s1 = "lttkjeerkhadffxqmfokcwdoicbxpydimobpcnkhzjcgpdigxanczxqw";
     char* s2 = "mbucihazqpoylmfiiturlclabwydqeegdajcmuhrodmfuaeuminwieekuihyuitlnjymjtyxzgokxaubeuovhsdyv";
+    */
+
+    char* s = "abcdde";
+    char* s1 = "baccd";
+    char* s2 = "eeabg";
 
     char inputedStrings[t][MAXLENGTH];
 
@@ -91,14 +99,11 @@ int main()
     }
     */
 
-    int controlLength = _strlen(inputedStrings[0]);
 
-    char controlString[controlLength];
-
-    int charMarkers[controlLength];
+    int charMarkers[ALPHABET_LENGTH + 1];
 
     //init charMarkers with 0-values
-    for(int i = 0; i < controlLength; i++)
+    for(int i = 0; i < ALPHABET_LENGTH; i++)
     {
         charMarkers[i] = 0;
     }
@@ -111,17 +116,47 @@ int main()
     }
     */
 
+    char controlString[_strlen(inputedStrings[0])];
     _strcopy(inputedStrings[0], controlString);
 
 
+    for(int i = 0; i < t; i++)
+    {
+        for(int j = 0; j < _strlen(inputedStrings[0]); j++)
+        {
+            if(indexOf(inputedStrings[i], controlString[j]) != -1)
+            {
+                charMarkers[controlString[j] - SMALL_A_ACII_INDEX]++;
+            }
+        }
+    }
 
+    /*
+    for(int i = 0; i < ALPHABET_LENGTH; i++)
+    {
+        printf("%i - %c - %i\n", i + 1, i + SMALL_A_ACII_INDEX, charMarkers[i]);
+    }
+    */
+
+    int totalCounter = 0;
+
+    for(int i = 0; i < ALPHABET_LENGTH; i++)
+    {
+        if(charMarkers[i] == t)
+        {
+            totalCounter++;
+        }
+    }
 
     //****************************************test is string copied correctly
     /*
     printf("%s\n", controlString);
     */
 
+    /*
+    printf("Total counter is %i\n", totalCounter);
+    */
 
-
+    printf("%i", totalCounter);
     return 0;
 }
